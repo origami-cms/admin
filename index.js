9f7ce600ee55dbf80f3a4ba11a260721320711ad
Tristan Matthias <hello@tristanmatthias.com>
2018-06-18 11:01:05 -0700

    Removed express as a dep, and changed to Origami plugin pattern

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
-const router = new express.Router();
+const {Route} = require('origami-core-server');
 
-module.exports = () => {
-    router.use(express.static(path.resolve(__dirname, 'build')));
-    router.use('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build/index.html'));
-    });
 
-    return router;
+
+module.exports = (server, options) => {
+    server.static(path.resolve(__dirname, './build'), '/admin');
+    const r = new Route('/admin/*')
+        .position('post-render')
+        .use(async (req, res, next) => {
+            const body = res.body || res.text || res.data || res.responseCode;
+            if (!res.headersSent && !body) res.sendFile(path.resolve(__dirname, 'build/index.html'));
+            else next();
+        });
+
+    server.useRouter(r);
 };
